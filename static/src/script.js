2ee8a0255b546e8be3585f9b394bbb9e573592c8
irvanshandika <shandikamuhammadirvan@gmail>
2024-06-24 22:18:03 +0700

    Delete unused code

--- static/src/script.js
+++ static/src/script.js
@@ -1,6 +1,3 @@
 const d = new Date();
 let year = d.getFullYear();
 document.getElementById("year").innerHTML = year;
-
-kofiwidget2.init("Support Me on Ko-fi", "#29abe0", "E1E0ZPGJQ");
-kofiwidget2.draw();
